7ff75797e84946ca061425c88181c20f0f0205ac
darkbeast69 <guptaaryan189@gmail.com>
2022-11-25 20:08:20 +0000

    Show ProjectFlexo Version in flashing script

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -264,17 +264,18 @@ class EdifyGenerator(object):
     """Log a message to the screen (if the logs are visible)."""
     self.script.append('ui_print("%s");' % (message,))
 
-  def PrintPixelExperienceBanner(self, is_plus, android_version, build_id, build_date,
+  def PrintPixelExperienceBanner(self, is_plus, android_version, flexo_version, build_id, build_date,
                                   security_patch, device):
     self.Print("----------------------------------------------")
     if is_plus:
-      self.Print("        PixelExperience (Plus edition)")
-      self.Print("                by jhenrique09")
+      self.Print("        ProjectFlexo(Reboot)")
+      self.Print("                by TeamFlexo")
     else:
-      self.Print("              PixelExperience")
-      self.Print("              by jhenrique09")
+      self.Print("              ProjectFlexo(Reboot)")
+      self.Print("              by TeamFlexo")
     self.Print("----------------------------------------------")
     self.Print(" Android version: %s"%(android_version))
+    self.Print(" ProjectFlexo version: %s"%(flexo_version))
     self.Print(" Build id: %s"%(build_id))
     self.Print(" Build date: %s"%(build_date))
     self.Print(" Security patch: %s"%(security_patch))
